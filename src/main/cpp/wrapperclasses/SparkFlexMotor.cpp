504b335e4f2ca84990e8d9b864393e0f267fb1c3
aplatt0202 <155834742+aplatt0202@users.noreply.github.com>
2025-01-06 18:50:36 -0500

    Motor wrapper classes (#2)
    * wrapperclasses
    
    * uncommented

---
+++ src/main/cpp/wrapperclasses/SparkFlexMotor.cpp
@@ -0,0 +1,87 @@
+#include "wrapperclasses/SparkFlexMotor.h"
+
+#include "wrapperclasses/SparkMaxMotor.h"
+
+// SparkMaxMotor::SparkMaxMotor(int id) //: motor{id, rev::spark::SparkLowLevel::MotorType::kBrushless} 
+// {
+//     //motor.Configure(config, rev::spark::SparkBase::ResetMode::kResetSafeParameters, rev::spark::SparkBase::PersistMode::kPersistParameters); 
+// }
+
+void SparkFlexMotor::SetPercent(double percent)
+{
+    motor.Set(percent); 
+}
+
+void SparkFlexMotor::StopMotor() 
+{
+    motor.Set(0); 
+}
+
+double SparkFlexMotor::GetRelativeVelocity() 
+{
+    RelativeEncoder.GetVelocity(); 
+}
+
+double SparkFlexMotor::GetRelativePosition()
+{
+    return RelativeEncoder.GetPosition(); 
+}
+
+double SparkFlexMotor::SetRelativePosition(double pos)
+{
+    RelativeEncoder.SetPosition(pos); 
+}
+
+void SparkFlexMotor::setPID(double p, double i, double d, double ff)
+{
+    ConfigParam.P = p; 
+    ConfigParam.I = i; 
+    ConfigParam.D = d; 
+    ConfigParam.ff = ff; 
+    closedLoopConfig.Pidf(p, i, d, ff); // add slot 
+}
+
+void SparkFlexMotor::setPID(double p, double i, double d)
+{
+    ConfigParam.P = p; 
+    ConfigParam.I = i; 
+    ConfigParam.D = d; 
+    ConfigParam.ff = 0; 
+    closedLoopConfig.Pidf(p, i, d, 0); // add slot 
+}
+
+void SparkFlexMotor::setForwardSoftLimit(double limit)
+{
+    ConfigParam.forwardSoftLimit = limit; 
+    softLimitConfig.ForwardSoftLimit(limit); 
+}
+
+void SparkFlexMotor::setReverseSoftLimit(double limit)
+{
+    ConfigParam.reverseSoftLimit = limit; 
+    softLimitConfig.ReverseSoftLimit(limit); 
+}
+
+void SparkFlexMotor::enableForwardSoftLimit(bool enab)
+{
+    ConfigParam.isForwardSoftLimitEnabled = enab; 
+    softLimitConfig.ForwardSoftLimitEnabled(enab); 
+}
+
+void SparkFlexMotor::enableReverseSoftLimit(bool enab)
+{
+    ConfigParam.isReverseSoftLimitEnabled = enab; 
+    softLimitConfig.ReverseSoftLimitEnabled(enab); 
+}
+
+void SparkFlexMotor::setInverted(bool b)
+{
+    config.Inverted(b); 
+}
+
+void SparkFlexMotor::configure() 
+{
+    config.Apply(closedLoopConfig); 
+    config.Apply(softLimitConfig); 
+}
+
---
+++ src/main/include/wrapperclasses/SparkFlexMotor.h
@@ -0,0 +1,48 @@
+# pragma once
+
+#include "wrapperclasses/Motor.h"
+
+#include <rev/SparkBase.h>
+#include<rev/SparkFlex.h> 
+
+class SparkFlexMotor : public Motor
+{
+    private: 
+        rev::spark::SparkFlex motor; 
+        rev::spark::SparkAbsoluteEncoder AbsoluteEncoder = motor.GetAbsoluteEncoder(); 
+        rev::spark::SparkRelativeEncoder RelativeEncoder = motor.GetEncoder(); 
+        rev::spark::SparkFlexExternalEncoder ExternalRelativeEncoder = motor.GetExternalEncoder(); 
+        rev::spark::SparkBaseConfig config; 
+        rev::spark::ClosedLoopConfig closedLoopConfig; 
+        rev::spark::SoftLimitConfig softLimitConfig; 
+
+    public: 
+        SparkFlexMotor(int id) : motor{id, rev::spark::SparkLowLevel::MotorType::kBrushless} 
+        {
+            setPID(0, 0, 0); 
+        }
+
+        
+
+        void SetPercent(double percent) override;
+        void StopMotor() override; 
+
+        double GetRelativeVelocity() override; 
+        double GetRelativePosition() override;
+        double SetRelativePosition(double pos) override;
+
+        // CONFIGURE SETTINGS // 
+
+        void configure() override; 
+        void setPID(double p, double i, double d, double ff) override; 
+        void setPID(double p, double i, double d) override; 
+
+        void setForwardSoftLimit(double limit) override; 
+        void setReverseSoftLimit(double limit) override; 
+        void enableForwardSoftLimit(bool enab) override; 
+        void enableReverseSoftLimit(bool enab) override; 
+
+        virtual void setInverted(bool b) override; 
+
+
+}; 
