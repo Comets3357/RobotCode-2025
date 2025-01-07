6244cc06d4cdf322325765182e71ac978273d98f
Cohen Cummings <cococohen7@icloud.com>
2025-01-06 19:05:23 -0500

    some small changes

--- src/main/include/Subsystems/ElbowSubsystem.h
+++ src/main/include/Subsystems/ElbowSubsystem.h
@@ -9,10 +9,28 @@ class ElbowSubsystem : public frc2::SubsystemBase {
 public:
 
     ElbowSubsystem();
+
+    //constants
+    
     
     //epic PID system for the elbow, dont change these values unless you know what you are doing :)
-    const double P = 1;
-    const double I = 0;
-    const double D = 0;
+    const double elbowP = 1;
+    const double elbowI = 0;
+    const double elbowD = 0;
+    frc::PivotPIDController{elbowP, elbowI, elbowD};
+
+    //targetAngle, pretty self-explainatory if you ask me.
+    int targetAngle = 0;
     
+    //setters
+    void setElbowAngle(int degree);
+    void setElbowSpeed(int speed);
+    
+    //getters
+    void getElbowAngle();
+    void getElbowSpeed();
+
+private:
+
+
 }
