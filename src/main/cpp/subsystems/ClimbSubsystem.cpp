9ec6f8c4ab162033b6b4d318d5a6bb48bb6405cb
aplatt0202 <155834742+aplatt0202@users.noreply.github.com>
2025-03-06 17:08:32 -0500

    Climb limits updated for new zero (#15)

--- src/main/cpp/subsystems/ClimbSubsystem.cpp
+++ src/main/cpp/subsystems/ClimbSubsystem.cpp
@@ -38,10 +38,10 @@ ClimbSubsystem::ClimbSubsystem(){
     climbMotor.setFeedbackSensor(Motor::encoderType::absolute);
     climbMotor.setAbsolutePositionConversionFactor(360);
     climbMotor.setAbsoluteVelocityConversionFactor(6);
-    climbMotor.setReverseSoftLimit(2);
-    climbMotor.setForwardSoftLimit(120);
-    climbMotor.enableForwardSoftLimit(false);
-    climbMotor.enableReverseSoftLimit(false);
+    climbMotor.setReverseSoftLimit(175); // absolute limit this is climb extension
+    climbMotor.setForwardSoftLimit(288); // absolute limit do not go farther than this
+    climbMotor.enableForwardSoftLimit(true);
+    climbMotor.enableReverseSoftLimit(true);
     climbMotor.setPID(0.02,0,0);
     //climbMotor.setAbsoluteEncoderInverted(true); 
     climbMotor.configure();
